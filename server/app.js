050d3d31b7d26dcb4918431e2e0ade00a7f4e773
skdksldk <tnwns1485@naver.com>
2021-08-26 19:51:12 +0900

    create react app

---
+++ server/app.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+
+
+var app = express();
+
+
+dotenv.config();
+
+connectDB();
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running on port ${PORT}`)
+  //console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
