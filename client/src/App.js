51f9c157c4ba61fec1b856486cc172ae1f5e8b95
skdksldk <tnwns1485@naver.com>
2021-08-28 19:12:44 +0900

    add loginpage

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
     return ( <
@@ -15,7 +16,11 @@ function App() {
         <
         Route exact path = '/'
         component = { HomePage }
-        /> < /
+        />  <
+        Route exact path = '/login'
+        component = { LoginPage }
+        /> <
+        /
         main >
 
         <
---
+++ client/src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import loadingGif from '../images/Loading.gif';
+
+const Loader = () => {
+    return <img src = { loadingGif }
+    className = 'loading'
+    alt = 'Loading' / > ;
+};
+
+export default Loader;
