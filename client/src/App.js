27d20925b03e7e24f5af30292d26bcd8efff604b
skdksldk <tnwns1485@naver.com>
2021-08-28 18:59:25 +0900

    add header

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <main>
-          <Route exact path='/' component={HomePage} />
-        </main>
+    return ( <
+        div className = "App" >
+        <
+        Router >
+        <
+        Header / >
+        <
+        main >
+        <
+        Route exact path = '/'
+        component = { HomePage }
+        /> < /
+        main >
 
-      </Router>
-    </div>
-  );
+        <
+        /Router> < /
+        div >
+    );
 }
 
 export default App;
