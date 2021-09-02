48c4abc9be142ab6c4fd0b5e83c90f5a9f5efa97
skdksldk <tnwns1485@naver.com>
2021-09-02 13:12:25 +0900

    modify productmanage design

--- client/src/pages/ProductManagePage.js
+++ client/src/pages/ProductManagePage.js
@@ -6,103 +6,131 @@ import Loader from '../components/Loader';
 import Pagination from '../components/Pagination';
 import { listProducts, deleteProduct } from '../actions/productActions';
 import Meta from '../components/Meta';
+import {
+    faEdit,
+    faTimes
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ProductManagePage = ({ history, match }) => {
-  const pageNumber = match.params.pageNumber || 1;
+        const pageNumber = match.params.pageNumber || 1;
 
-  const dispatch = useDispatch();
+        const dispatch = useDispatch();
 
-  const productList = useSelector(state => state.productList);
-  const { loading, error, products, pages, page } = productList;
+        const productList = useSelector(state => state.productList);
+        const { loading, error, products, pages, page } = productList;
 
-  const productDelete = useSelector(state => state.productDelete);
-  const {
-    loading: loadingDelete,
-    error: errorDelete,
-    success: successDelete,
-  } = productDelete;
+        const productDelete = useSelector(state => state.productDelete);
+        const {
+            loading: loadingDelete,
+            error: errorDelete,
+            success: successDelete,
+        } = productDelete;
 
-  const userLogin = useSelector(state => state.userLogin);
-  const { userInfo } = userLogin;
+        const userLogin = useSelector(state => state.userLogin);
+        const { userInfo } = userLogin;
 
-  useEffect(() => {
-    if (!userInfo.isAdmin) {
-      history.push('/login');
-    }
+        useEffect(() => {
+            if (!userInfo.isAdmin) {
+                history.push('/login');
+            }
 
-    dispatch(listProducts('', '', pageNumber));
-  }, [dispatch, history, userInfo, successDelete, pageNumber]);
+            dispatch(listProducts('', '', pageNumber));
+        }, [dispatch, history, userInfo, successDelete, pageNumber]);
 
-  const deleteHandler = id => {
-    if (window.confirm('Are you sure?')) {
-      dispatch(deleteProduct(id));
-    }
-  };
+        const deleteHandler = id => {
+            if (window.confirm('Are you sure?')) {
+                dispatch(deleteProduct(id));
+            }
+        };
 
-  return (
-    <div className='container'>
-      <Meta title='Products List | ADMIN PAGE' />
-      <h3 className='admin__product__title'>PRODUCTS</h3>
+        return ( <
+            div className = 'container' >
+            <
+            Meta title = 'Products List | ADMIN PAGE' / >
+            <
+            h3 className = 'admin__product__title' > 상품관리 < /h3>
 
-      <Link to='/admin/product/create' className='btn'>
-        <i className='fas fa-plus'></i> CREATE PRODUCT
-      </Link>
+            <
+            Link to = '/admin/product/create'
+            className = 'btn' >
+            상품 추가 <
+            /Link>
 
-      <div className='admin__product'>
-        {loadingDelete && <Loader />}
-        {errorDelete && <Message className='error'>{errorDelete}</Message>}
+            <
+            div className = 'admin__product' > { loadingDelete && < Loader / > } {
+                errorDelete && < Message className = 'error' > { errorDelete } < /Message>}
 
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <div className='error'>
-            <Message>{error}</Message>
-          </div>
-        ) : (
-          <table className='admin__product__table'>
-            <thead>
-              <tr>
-                <th>NO.</th>
-                <th>NAME</th>
-                <th>PRICE</th>
-                <th>CATEGORY</th>
-                <th>BRAND</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products &&
-                products.map((product, index) => (
-                  <tr key={product._id}>
-                    <td>{index + 1}</td>
-                    <td>{product.name}</td>
-                    <td>${product.price}</td>
-                    <td>{product.category}</td>
-                    <td>{product.brand}</td>
+                {
+                    loading ? ( <
+                        Loader / >
+                    ) : error ? ( <
+                        div className = 'error' >
+                        <
+                        Message > { error } < /Message> <
+                        /div>
+                    ) : ( <
+                        table className = 'admin__product__table' >
+                        <
+                        thead >
+                        <
+                        tr >
+                        <
+                        th > 순번 < /th> <
+                        th > 이름 < /th> <
+                        th > 가격 < /th> <
+                        th > 카테고리 < /th> <
+                        th > 브랜디 < /th> <
+                        th > 수정 / 삭제 < /th> <
+                        /tr> <
+                        /thead> <
+                        tbody > {
+                            products &&
+                            products.map((product, index) => ( <
+                                tr key = { product._id } >
+                                <
+                                td > { index + 1 } < /td> <
+                                td > { product.name } < /td> <
+                                td > $ { product.price } < /td> <
+                                td > { product.category } < /td> <
+                                td > { product.brand } < /td>
 
-                    <td className='admin__product__btn'>
-                      <Link to={`/admin/product/${product._id}/edit`}>
-                        <button className='btn admin__product__edit'>
-                          <i className='fas fa-edit'></i>
-                        </button>
-                      </Link>
-                      <button
-                        className='btn'
-                        onClick={() => deleteHandler(product._id)}
-                      >
-                        <i className='fas fa-trash'></i>
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        )}
-      </div>
-      <div>
-        <Pagination pages={pages} page={page} isAdmin={true} />
-      </div>
-    </div>
-  );
-};
+                                <
+                                td className = 'admin__product__btn' >
+                                <
+                                Link to = { `/admin/product/${product._id}/edit` } >
+                                <
+                                button className = 'btn admin__product__edit' >
+                                <
+                                FontAwesomeIcon icon = { faEdit }
+                                /> <
+                                /button> <
+                                /Link> <
+                                button className = 'btn'
+                                onClick = {
+                                    () => deleteHandler(product._id) } >
+                                <
+                                FontAwesomeIcon icon = { faTimes }
+                                /> <
+                                /button> <
+                                /td> <
+                                /tr>
+                            ))
+                        } <
+                        /tbody> <
+                        /table>
+                    )
+                } <
+                /div> <
+                div >
+                    <
+                    Pagination pages = { pages }
+                page = { page }
+                isAdmin = { true }
+                /> <
+                /div> <
+                /div>
+            );
+        };
 
-export default ProductManagePage;
+        export default ProductManagePage;
