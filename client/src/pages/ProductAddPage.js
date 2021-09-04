9fd7da3e64a71e909bccd8bf18b68a832d580608
skdksldk <tnwns1485@naver.com>
2021-09-04 15:14:09 +0900

    modify product management and add

--- client/src/pages/ProductAddPage.js
+++ client/src/pages/ProductAddPage.js
@@ -9,188 +9,192 @@ import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
 import Meta from '../components/Meta';
 
 const ProductAddPage = ({ history }) => {
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState(0);
-    const [image, setImage] = useState('');
-    const [brand, setBrand] = useState('');
-    const [category, setCategory] = useState('tops');
-    const [countInStock, setCountInStock] = useState(0);
-    const [description, setDescription] = useState('');
-    const [uploading, setUploading] = useState(false);
+        const [name, setName] = useState('');
+        const [price, setPrice] = useState(0);
+        const [image, setImage] = useState('');
+        const [brand, setBrand] = useState('');
+        const [category, setCategory] = useState('outers');
+        const [countInStock, setCountInStock] = useState(0);
+        const [description, setDescription] = useState('');
+        const [uploading, setUploading] = useState(false);
 
-    const dispatch = useDispatch();
+        const dispatch = useDispatch();
 
-    const productCreate = useSelector(state => state.productCreate);
-    const {
-        loading: loadingCreate,
-        error: errorCreate,
-        success: successCreate,
-    } = productCreate;
+        const productCreate = useSelector(state => state.productCreate);
+        const {
+            loading: loadingCreate,
+            error: errorCreate,
+            success: successCreate,
+        } = productCreate;
 
-    useEffect(() => {
-        if (successCreate) {
-            dispatch({ type: PRODUCT_CREATE_RESET });
-            history.push('/admin/productlist');
-        }
-    }, [dispatch, history, successCreate]);
+        useEffect(() => {
+            if (successCreate) {
+                dispatch({ type: PRODUCT_CREATE_RESET });
+                history.push('/admin/productlist');
+            }
+        }, [dispatch, history, successCreate]);
 
-    const uploadFileHandler = async e => {
-        const file = e.target.files[0];
-        const formData = new FormData();
-        formData.append('image', file);
-        setUploading(true);
+        const uploadFileHandler = async e => {
+            const file = e.target.files[0];
+            const formData = new FormData();
+            formData.append('image', file);
+            setUploading(true);
 
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            };
-            const { data } = await axios.post('/api/upload', formData, config);
-            setImage(data);
-            setUploading(false);
-        } catch (error) {
-            console.error(error);
-            setUploading(false);
-        }
-    };
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                };
+                const { data } = await axios.post('/api/upload', formData, config);
+                setImage(data);
+                setUploading(false);
+            } catch (error) {
+                console.error(error);
+                setUploading(false);
+            }
+        };
 
-    const submitHandler = e => {
-        e.preventDefault();
-        dispatch(
-            createProduct({
-                name,
-                price,
-                image,
-                brand,
-                category,
-                countInStock,
-                description,
-            })
-        );
-    };
+        const submitHandler = e => {
+            e.preventDefault();
+            dispatch(
+                createProduct({
+                    name,
+                    price,
+                    image,
+                    brand,
+                    category,
+                    countInStock,
+                    description,
+                })
+            );
+        };
 
-    return ( <
-        FormContainer >
-        <
-        Meta title = 'Create Product' / >
-        <
-        h3 > 상품 추가 < /h3> { loadingCreate && < Loader / > } {
-            errorCreate && ( <
-                div className = 'error' >
+        return ( <
+                FormContainer >
                 <
-                Message > { errorCreate } < /Message> <
-                /div>
-            )
-        } <
-        div className = 'product__create' >
-        <
-        form onSubmit = { submitHandler } >
-        <
-        table >
-        <
-        tr >
-        <
-        td > 상품명 < /td> <
+                Meta title = 'Create Product' / >
+                <
+                h3 > 상품 추가 < /h3> { loadingCreate && < Loader / >
+            } {
+                errorCreate && ( <
+                    div className = 'error' >
+                    <
+                    Message > { errorCreate } < /Message> < /
+                    div >
+                )
+            } <
+            div className = 'common__form' >
+            <
+            form onSubmit = { submitHandler } >
+            <
+            table >
+            <
+            tr >
+            <
+            td > 상품명 < /td> <
         td >
-        <
-        input type = "name"
+            <
+            input type = "name"
         placeholder = '상품명 입력..'
         value = { name }
         onChange = { e => setName(e.target.value) }
         /><br / >
         <
-        /td> <
-        /tr> <
-        tr >
-        <
-        td > 가격 < /td> <
+        /td> < /
+        tr > <
+            tr >
+            <
+            td > 가격 < /td> <
         td >
-        <
-        input type = 'number'
+            <
+            input type = 'number'
         placeholder = '가격 입력..'
         value = { price }
         onChange = { e => setPrice(e.target.value) }
-        /> <
-        /td> <
-        /tr> <
+        /> < /
+        td > <
+            /tr> <
         tr >
-        <
-        td > 이미지 < /td> <
+            <
+            td > 이미지 < /td> <
         td >
-        <
-        input type = 'text'
+            <
+            input type = 'text'
         placeholder = '이미지 선택..'
         value = { image }
         onChange = { e => setImage(e.target.value) }
         /> <
         br / >
-        <
-        input type = 'file'
+            <
+            input type = 'file'
         onChange = { uploadFileHandler }
-        /> { uploading && < Loader / > } <
-        /td> <
-        /tr> <
-        tr >
-        <
-        td > 브랜드 < /td> <
-        td >
-        <
-        input type = 'text'
-        value = { brand }
-        onChange = { e => setBrand(e.target.value) }
-        /> <
-        /td> <
-        /tr> <
-        tr >
-        <
-        td > 재고수 < /td> <
-        td >
-        <
-        input type = 'number'
-        placeholder = '재고수 입력..'
-        value = { countInStock }
-        onChange = { e => setCountInStock(e.target.value) }
-        /> <
-        /td> <
-        /tr> <
-        tr >
-        <
-        td > 종류 < /td> <
-        td >
-        <
-        select id = "selbox"
-        name = "selbox"
-        onChange = { e => setCategory(e.target.value) } >
-        <
-        option value = "tops"
-        selected > TOPS < /option> <
-        option value = "pants" > PANTS < /option> <
-        option value = "caps" > CAPS < /option> <
-        /select> <
-        /td> <
-        /tr> <
-        tr >
-        <
-        td > 요약 < /td> <
-        td >
-        <
-        input type = 'text'
-        value = { description }
-        onChange = { e => setDescription(e.target.value) }
-        /> <
-        br / >
-        <
-        /td> <
-        /tr> <
-        /table>
+        /> { uploading && < Loader / >
+    } <
+    /td> < /
+    tr > <
+    tr >
+    <
+    td > 브랜드 < /td> <
+td >
+    <
+    input type = 'text'
+value = { brand }
+onChange = { e => setBrand(e.target.value) }
+/> < /
+td > <
+    /tr> <
+tr >
+    <
+    td > 재고수 < /td> <
+td >
+    <
+    input type = 'number'
+placeholder = '재고수 입력..'
+value = { countInStock }
+onChange = { e => setCountInStock(e.target.value) }
+/> < /
+td > <
+    /tr> <
+tr >
+    <
+    td > 종류 < /td> <
+td >
+    <
+    select id = "selbox"
+name = "selbox"
+onChange = { e => setCategory(e.target.value) } >
+    <
+    option value = "outers"
+selected > OUTERS < /option> <
+    option value = "tops" > TOPS < /option> <
+    option value = "pants" > PANTS < /option> <
+    option value = "dresses" > DRESSES < /option> <
+option value = "caps" > CAPS < /option> < /
+    select > <
+    /td> < /
+    tr > <
+    tr >
+    <
+    td > 요약 < /td> <
+td >
+    <
+    input type = 'text'
+value = { description }
+onChange = { e => setDescription(e.target.value) }
+/> <
+br / >
+    <
+    /td> < /
+    tr > <
+    /table>
 
-        <
-        button className = 'btn' > 상품 추가 < /button> <
-        /form> <
-        /div> <
-        /FormContainer>
-    );
+<
+button className = 'btn' > 상품 추가 < /button> < /
+    form > <
+    /div> < /
+    FormContainer >
+);
 };
 
 export default ProductAddPage;
