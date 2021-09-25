3e7f971ad9b57979366e547cff3bae84f2db78ef
skdksldk <tnwns1485@naver.com>
2021-09-25 15:23:08 +0900

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import MyPage from './pages/MyPage';
@@ -21,89 +22,41 @@ import OrderCheckPage from './pages/OrderCheckPage';
 import OrderListPage from './pages/OrderListPage';
 
 
-function App() {
-    return ( <
-        div className = "wrapper" >
-        <
-        Router >
-        <
-        Header / >
-        <
-        Nav / >
-        <
-        main >
-        <
-        Route exact path = '/'
-        component = { HomePage }
-        /> <
-        Route exact path = '/login'
-        component = { LoginPage }
-        /> <
-        Route exact path = '/register'
-        component = { RegisterPage }
-        /> <
-        Route exact path = '/profile'
-        component = { MyPage }
-        /> <
-        Route exact path = '/admin/userlist'
-        component = { UserListPage }
-        /> <
-        Route exact path = '/shop'
-        component = { ProductsPage }
-        /> <
-        Route exact path = '/shop/page/:pageNumber'
-        component = { ProductsPage }
-        /> <
-        Route exact path = '/admin/productlist'
-        component = { ProductManagePage }
-        /> <
-        Route exact path = '/admin/productlist/:pageNumber'
-        component = { ProductManagePage }
-        /> <
-        Route exact path = '/admin/product/create'
-        component = { ProductAddPage }
-        /> <
-        Route exact path = '/admin/product/:id/edit'
-        component = { ProductEditPage }
-        /> <
-        Route exact path = '/shop/category/:category'
-        component = { ProductsPage }
-        /> <
-        Route exact path = '/shop/product/:id'
-        component = { ProductDetail }
-        /> <
-        Route exact path = '/shop/search'
-        component = { SearchPage }
-        /> <
-        Route exact path = '/shop/search/:keyword'
-        component = { SearchPage }
-        /> <
-        Route exact path = '/shop/search/:keyword/page/:pageNumber'
-        component = { SearchPage }
-        /> <
-        Route exact path = '/cart/:id?'
-        component = { CartPage }
-        /> <
-        Route exact path = '/shipping'
-        component = { OrderPage }
-        /> <
-        Route exact path = '/payment'
-        component = { OrderPayPage }
-        /> <
-        Route exact path = '/placeorder'
-        component = { OrderPlacePage }
-        /> <
-        Route exact path = '/order/:id'
-        component = { OrderCheckPage }
-        /> <
-        Route exact path = '/admin/orderlist'
-        component = { OrderListPage }
-        /> < /
-        main >
 
-        <
-        /Router> < /
-        div >
+function App() {
+    return ( 
+    <div className = "wrapper" >
+      <Router >
+     <Header/>
+    <Nav/>
+    <main>
+    <Route exact path = '/'  component = { HomePage }/>  
+    <Route exact path = '/login' component = { LoginPage }/>  
+    <Route exact path = '/register' component = { RegisterPage }/>  
+    <Route exact path = '/profile' component = { MyPage }/>  
+    <Route exact path = '/admin/userlist' component = { UserListPage }/>  
+    <Route exact path = '/shop' component = { ProductsPage }/>  
+    <Route exact path = '/shop/page/:pageNumber' component = { ProductsPage }/>  
+    <Route exact path = '/admin/productlist' component = { ProductManagePage }/>  
+    <Route exact path = '/admin/productlist/:pageNumber' component = { ProductManagePage }/>  
+    <Route exact path = '/admin/product/create' component = { ProductAddPage }/>  
+    <Route exact path = '/admin/product/:id/edit' component = { ProductEditPage }/>  
+    <Route exact path = '/shop/category/:category' component = { ProductsPage }/>  
+    <Route exact path = '/shop/product/:id' component = { ProductDetail }/>  
+    <Route exact path = '/shop/search' component = { SearchPage }/>  
+    <Route exact path = '/shop/search/:keyword' component = { SearchPage }/>  
+    <Route exact path = '/shop/search/:keyword/page/:pageNumber' component = { SearchPage }/>  
+    <Route exact path = '/cart/:id?' component = { CartPage }/>  
+    <Route exact path = '/shipping' component = { OrderPage }/>  
+    <Route exact path = '/payment' component = { OrderPayPage }/>  
+    <Route exact path = '/placeorder' component = { OrderPlacePage }/>  
+    <Route exact path = '/order/:id' component = { OrderCheckPage }/>  
+    <Route exact path = '/admin/orderlist' component = { OrderListPage }/> 
+    <Route exact path = '/' component = { HomePage }/>  
+    </main> 
+    <Route path='/' render={({ location }) => location.pathname !== '/' && <Footer />}/>
+      </Router>  
+    </div>
     );
 }
 
---
+++ client/src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <ul>
+        <li>
+          OPENING HOURS. AM 9:30-PM 5:30
+          <br />
+          토.일.공휴일은 휴일입니다
+          <br />
+          <br />
+          ADDRESS 인천광역시 연구수 옥련동 현대인프라
+          <br />
+          E-MAIL tnwns1485@naver.com
+        </li>
+        <li>
+          <strong>COMPANY JUN</strong>
+          <br />
+          OWNER & ADMIN JUN KIM
+          <br />
+          ONLINE ORDER LICENSE 2021-JUNkim-1253
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
